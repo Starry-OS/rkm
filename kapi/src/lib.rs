e61c0a4961cc57129de9ede2c0628a947ed34684
Godones <chenlinfeng25@outlook.com>
2026-03-26 19:40:42 +0800

    Add feature flags for modular kernel API selection
    Signed-off-by: Godones <chenlinfeng25@outlook.com>

--- kapi/src/lib.rs
+++ kapi/src/lib.rs
@@ -5,11 +5,18 @@ extern crate alloc;
 
 use axerrno::{LinuxError, LinuxResult};
 
+#[allow(dead_code)]
 type Result<T> = LinuxResult<T>;
+#[allow(dead_code)]
 type ModuleErr = LinuxError;
 
+#[cfg(feature = "kstr")]
 pub mod kstrtox;
+#[cfg(feature = "kmem")]
 pub mod mm;
+#[cfg(feature = "kparameter")]
 pub mod param;
+#[cfg(feature = "kstr")]
 pub mod string;
+#[cfg(feature = "kstr")]
 pub mod string_helper;
